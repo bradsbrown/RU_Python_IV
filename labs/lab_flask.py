7f80d44e2a398af0388c972a049d11fbc75f11c9
Brad Brown <brad.brown@rackspace.com>
2018-12-12 15:58:17 -0600

    lab flask server

--- labs/lab_flask.py
+++ labs/lab_flask.py
@@ -21,3 +21,44 @@ LAB_FLASK Learning Objective: Learn to serve RESTful APIs using the Flask librar
     in the url.  For instance, /centigrade/0.0 should return "32.0"
 
 """
+
+import datetime
+
+from flask import Flask
+from werkzeug.routing import BaseConverter
+
+
+class FlexiNumber(BaseConverter):
+    def to_python(self, value):
+        return float(value)
+
+    def to_url(self, value):
+        return f"{value:.2f}"
+
+app = Flask(__name__)
+app.url_map.converters['number'] = FlexiNumber
+
+
+
+@app.route('/')
+def root():
+    return "<h1>Welcome to my server</h1>"
+
+
+@app.route("/now")
+def get_time():
+    return str(datetime.datetime.now())
+
+
+@app.route("/f/<number:temp>")
+@app.route("/fahrenheit/<number:temp>")
+def f_to_c(temp):
+    return f"{(5 * (temp - 32)) / 9:.2f}"
+
+@app.route("/c/<number:temp>")
+@app.route("/celsius/<number:temp>")
+def c_to_f(temp):
+    return f"{((9 * temp) / 5) + 32:.2f}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
