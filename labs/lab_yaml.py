99e2178c758abaac3d6d59a715dd757db8707aac
Brad Brown <brad.brown@rackspace.com>
2018-12-12 10:17:52 -0600

    add yaml lab code/result

--- labs/lab_yaml.py
+++ labs/lab_yaml.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # *-* coding:utf-8 *-*
 
+import yaml
+
 """
 
 :mod:`lab_yaml` -- YAML Parsing
@@ -19,3 +21,32 @@ LAB_YAML Learning Objective: Learn to parse a YAML file using the PyYAML library
  c. Save your updated object to widget_updated.yaml using the PyYAML library.
 
 """
+
+YAML_SOURCE_PATH = 'data/widget.yml'
+YAML_TARGET_PATH = 'data/widget_updated.yml'
+
+
+def read_yaml(path):
+    with open(path) as f:
+        return yaml.load(f.read())
+
+
+def write_yaml(data, path):
+    with open(path, 'w') as f:
+        f.write(yaml.dump(data, default_flow_style=False))
+
+
+def main():
+    data = read_yaml(YAML_SOURCE_PATH)
+
+    widget = data['widget']
+    widget['window']['height'] = widget['window']['height'] // 2
+    widget['window']['width'] = widget['window']['width'] // 2
+    widget['text']['size'] = widget['text']['size'] // 4
+    widget['image']['alignment'] = 'justified'
+
+    write_yaml(data, YAML_TARGET_PATH)
+
+
+if __name__ == '__main__':
+    main()
